ef55f8e1baf323f553a64d073fa20c4c98d7fd5a
Katy Kane <katytheartist@gmail.com>
2024-05-07 14:17:53 -0500

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,37 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model');
+
+async function validateActionId(req, res, next) {
+    try {
+        const action = await Actions.get(req.params.id)
+        if(!action){
+            res.status(404).json({
+                message: 'no such action known'
+            })
+        } else {
+            req.action = action;
+            next()
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            customMessage: 'problem finding action with that id',
+            message: err.message,
+            stack: err.stack,
+        })
+    }
+}
+
+function validateActions(req, res, next) {
+    const { notes, description, completed} = req.body;
+    if (!notes || !notes.trim() || !description || !description.trim() || typeof completed !== 'boolean') {
+        res.status(400).json({ message: 'the request body is missing notes, description or project_id' })
+    } else {
+        req.notes = notes;
+        req.description = description;
+        next();
+    }
+}
+
+module.exports = {
+    validateActionId, 
+    validateActions}
