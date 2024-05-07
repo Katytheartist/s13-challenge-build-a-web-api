e3e95e7f29f6683455fd086e272bad0f060da96b
Katy Kane <katytheartist@gmail.com>
2024-05-07 12:20:04 -0500

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -31,10 +31,8 @@ router.get('/:id', validateProjectId,  (req, res)=>{
 })
 
 router.post('/', validateProject, (req, res, next)=>{
-    Project.insert({
-        name: req.name, 
-        description: req.description,
-    })
+    const newProjectData = {name: req.name, description: req.description, completed: true}
+    Project.insert(newProjectData)
     .then(newProject =>{
         //throw new Error('danggit')
         res.status(201).json(newProject)
@@ -42,20 +40,39 @@ router.post('/', validateProject, (req, res, next)=>{
     .catch(next)
 })
 
-// router.delete('/:id', validateProjectId, (req, res)=>{
+router.delete('/:id', validateProjectId, async (req, res, next)=>{
+    try{
+        await Project.remove(req.params.id);
+        res.json(req.project);
+    } catch(err){
+        next(err)
+    }
+})
 
-// })
-
-// router.put('/:id', validateProjectId, (req, res)=>{
-
-// })
+router.put('/:id', validateProjectId, validateProject, (req, res, next)=>{
+    const projectInfo = {name: req.name, description: req.description, completed: req.completed}
+    Project.update(req.params.id, req.body)
+    .then(()=>{
+        return Project.get(req.params.id)
+    })
+    .then(updatedProject=>{
+        if(!updatedProject){
+            res.status(400).json({
+                message: 'update failed because missing name or description or completion status'
+            })
+        } else {
+            res.json(updatedProject)
+        }
+    })
+    .catch(next)
+})
 
 // router.get('/:id/actions', validateProjectId, (req, res)=>{
 
 // })
 
 
-router.use((err, req, res, next)=>{ 
+router.use((err, req, res, next)=>{ //eslint-disable-line
     res.status(err.status || 500).json({
         customMessage: 'A tragedy occurred in projects router!', 
         message: err.message,
